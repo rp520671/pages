164a672ed422a184b0a6273b553cc72195185d36
rp520671 <rp520671@gmail.com>
2023-04-14 14:51:25 +0530

    done

--- src/components/Accessllr.js
+++ src/components/Accessllr.js
@@ -4,20 +4,17 @@ import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Echo from './Echo';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { InputLabel,FormControl } from '@mui/material';
 import Button from "react-bootstrap/Button";
-import { Col, Row } from 'react-bootstrap';
-
+import Stack from "react-bootstrap/Stack";
+import { Save2, XSquareFill, CardChecklist } from 'react-bootstrap-icons';
 
 const Accessllr = () => {
     const [App, setApp] = React.useState('');
@@ -25,83 +22,87 @@ const Accessllr = () => {
     const handleChange = (event) => {
       setApp(event.target.value);
 };
+
   return (
     <Container >
       <Box >
-          <Paper elevation={6}>
-            <div>
+        <Paper elevation={6}>
+        
           <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-            
+          <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+          >  
             <div><h2>Access Level List</h2>
             <h6>define & update application access level</h6> </div>
-          
-          
           </AccordionSummary>
-        <AccordionDetails>
+         
+        <div >
         <Box
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          display:'flex',
+          flexDirection: 'row',
+        
+          p:1,
+          m:1,
+
+          borderRadius:1,
+          alignItems:'start',
+          '& > :not(style)': { m: 1, width: '25ch' }, 
         }}
         noValidate
         autoComplete="off"
-      >
-        <TextField id="Model" label="AccessLevelModel" variant="standard"/>   
+        >
+          
+
+
+       
         <TextField id="Name" label="AccessLevelName" variant="standard" />
         <TextField id="Description" label="AccessLevelDescription" variant="standard" />
+        <TextField id="outlined-multiline-static" label="AccessLevelModel" multiline rows={2} variant='standard' />   
 
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 10}}>
-        <InputLabel id="demo-simple-select-standard-label">App</InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={App}
-          onChange={handleChange}
-          label="App"
-        >
+        
+        <FormControl variant='standard'>
+          <InputLabel id="demo-standard-simple-select-label">App</InputLabel>
+        <Select labelId="demo-standard-simple-select-standard-label" id="demo-s imple-select-standard" value={App} onChange={handleChange} label="App">
           <MenuItem value="">
-            <em>None</em>
+          <em>None</em>
           </MenuItem>
           <MenuItem value={"FireQube"}>FireQube</MenuItem>
           <MenuItem value={"Laravel"}>Laravel</MenuItem>
-          
         </Select>
-        <FormControlLabel
-          value="Active"
-          control={<Checkbox />}
-          label="Active"
-          labelPlacement="end"
-        />
-        <Container>
-          <Row>
-            <Col>
-            <Button size="sm" variant="primary"> Save</Button></Col>
-        <Col>
-        <Button size="sm" variant="primary">Small</Button></Col>
-     
-      </Row>
+        </FormControl>
 
-        </Container>
-
-      </FormControl>
-    </Box> 
- </AccordionDetails>
-</Accordion>
-<Echo/>
-
+      <FormControlLabel value="Active" control={<Checkbox />} label="Active" labelPlacement="end"/>
+    
       
+      </Box> 
       
-       </div>     
-</Paper>
-</Box>
-</Container>
+        <Box sx={{p:1, m:1}}>
+         <Stack direction='horizontal' gap={2}> 
+                  <Button size="sm" variant="primary">
+                    <Save2 /> Save
+                  </Button>
+                  <Button size="sm" variant="primary"><XSquareFill/> Delete
+                  </Button>
+                  <Button size="sm" variant="primary"><CardChecklist/> Back to list
+                  </Button>
+                </Stack>
+         </Box> 
         
-     
+      </div>
+    </Accordion>
+    <Accordion>
+         
+
+    </Accordion>
+       
+   </Paper>
+  </Box>
+</Container>  
+
   )
 }
 
